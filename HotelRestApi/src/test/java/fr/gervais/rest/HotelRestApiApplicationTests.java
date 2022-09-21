35e0ecec30a55fed059022ef2adad81b774ba780
Gervais Pierre <94633856+GervaisPierre@users.noreply.github.com>
2022-09-21 10:05:29 +0200

    Initial

---
+++ HotelRestApi/src/test/java/fr/gervais/rest/HotelRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.gervais.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
